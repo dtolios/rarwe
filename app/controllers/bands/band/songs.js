07cadcdb9768341ec62806986d865ed30df2700a
Dimitri Tolios <dtolios09@gmail.com>
2019-02-09 14:34:39 -0800

    add search and sort

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,32 +1,57 @@
-import Controller from '@ember/controller';
-import { empty } from '@ember/object/computed';
+import Controller from "@ember/controller";
+import { empty, sort } from "@ember/object/computed";
+import { computed } from "@ember/object";
 
 export default Controller.extend({
+  queryParams: {
+    sortBy: "sort",
+    searchTerm: "s"
+  },
   isAddingSong: false,
-  newSongTitle: '',
-  isAddButtonDisabled: empty('newSongTitle'),
+  newSongTitle: "",
+  isAddButtonDisabled: empty("newSongTitle"),
+  sortBy: "ratingDesc",
+  sortedSongs: sort("matchingSongs", "sortProperties"),
+  searchTerm: "",
+
+  matchingSongs: computed("model.songs.@each.title", "searchTerm", function() {
+    let searchTerm = this.searchTerm.toLowerCase();
+    return this.model.get("songs").filter(song => {
+      return song.title.toLowerCase().includes(searchTerm);
+    });
+  }),
+
+  sortProperties: computed("sortBy", function() {
+    let options = {
+      ratingDesc: ["rating:desc", "title:asc"],
+      ratingAsc: ["rating:asc", "title:asc"],
+      titleDesc: ["title:desc"],
+      titleAsc: ["title:asc"]
+    };
+    return options[this.sortBy];
+  }),
 
   actions: {
     addSong() {
-      this.set('isAddingSong', true);
+      this.set("isAddingSong", true);
     },
 
     cancelAddSong() {
-      this.set('isAddingSong', false);
+      this.set("isAddingSong", false);
     },
 
     async saveSong(event) {
       event.preventDefault();
-      let newSong = this.store.createRecord('song', {
-        title: this.get('newSongTitle'),
+      let newSong = this.store.createRecord("song", {
+        title: this.get("newSongTitle"),
         band: this.model
       });
       await newSong.save();
-      this.set('newSongTitle', '');
+      this.set("newSongTitle", "");
     },
 
     updateRating(song, rating) {
-      song.set('rating', song.rating === rating ? 0 : rating);
+      song.set("rating", song.rating === rating ? 0 : rating);
       song.save();
     }
   }
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from "qunit";
-import { visit, click } from "@ember/test-helpers";
+import { visit, click, fillIn, currentURL } from "@ember/test-helpers";
 import { createBand, createSong } from "rarwe/tests/helpers/custom-helpers";
 import { setupApplicationTest } from "ember-qunit";
 import setupMirageTest from "ember-cli-mirage/test-support/setup-mirage";
@@ -56,5 +56,131 @@ module("Acceptance | Bands", function(hooks) {
     assert
       .dom("[data-test-rr=song-list-item]:last-child")
       .hasText("The Bends", "The new song is rendered as the last item");
+  });
+
+  test("Sort songs in various ways", async function(assert) {
+    let band = this.server.create("band", { name: "Them Crooked Vultures" });
+    this.server.create("song", { title: "Elephants", rating: 2, band });
+    this.server.create("song", { title: "New Fang", rating: 5, band });
+    this.server.create("song", {
+      title: "Mind Eraser, No Chaser",
+      rating: 1,
+      band
+    });
+    this.server.create("song", {
+      title: "Spinning in Daffodils",
+      rating: 3,
+      band
+    });
+    await visit("/");
+    await click("[data-test-rr=band-link]");
+    assert.equal(currentURL(), "/bands/1/songs");
+
+    assert
+      .dom("[data-test-rr=song-list-item]:first-child")
+      .hasText(
+        "New Fang",
+        "The first song is the highest ranked, first one in the alphabet"
+      );
+    assert
+      .dom("[data-test-rr=song-list-item]:last-child")
+      .hasText(
+        "Mind Eraser, No Chaser",
+        "The last song is the lowest ranked, last one in the alphabet"
+      );
+
+    await click("[data-test-rr=sort-by-title-desc]");
+    assert.equal(currentURL(), '/bands/1/songs?sort=titleDesc');
+    assert
+      .dom("[data-test-rr=song-list-item]:first-child")
+      .hasText(
+        "Spinning in Daffodils",
+        "The first song is the one that comes last in the alphabet"
+      );
+    assert
+      .dom("[data-test-rr=song-list-item]:last-child")
+      .hasText(
+        "Elephants",
+        "The last song is the one that comes first in the alphabet"
+      );
+
+    await click("[data-test-rr=sort-by-title-asc]");
+    assert.equal(currentURL(), '/bands/1/songs?sort=titleAsc');
+    assert
+      .dom("[data-test-rr=song-list-item]:first-child")
+      .hasText(
+        "Elephants",
+        "The first song is the one that comes first in the alphabet"
+      );
+    assert
+      .dom("[data-test-rr=song-list-item]:last-child")
+      .hasText(
+        "Spinning in Daffodils",
+        "The last song is the one that comes last in the alphabet"
+      );
+
+    await click("[data-test-rr=sort-by-rating-asc]");
+    assert.equal(currentURL(), '/bands/1/songs?sort=ratingAsc');
+    assert
+      .dom("[data-test-rr=song-list-item]:first-child")
+      .hasText(
+        "Mind Eraser, No Chaser",
+        "The first song is the one that has the lowest rating"
+      );
+    assert
+      .dom("[data-test-rr=song-list-item]:last-child")
+      .hasText(
+        "New Fang",
+        "The last song is the one that has the highest rating"
+      );
+  });
+
+  test("Search songs", async function(assert) {
+    let band = this.server.create("band", { name: "Them Crooked Vultures" });
+    this.server.create("song", { title: "Elephants", rating: 5, band });
+    this.server.create("song", { title: "New Fang", rating: 4, band });
+    this.server.create("song", {
+      title: "Mind Eraser, No Chaser",
+      rating: 4,
+      band
+    });
+    this.server.create("song", {
+      title: "Spinning in Daffodils",
+      rating: 5,
+      band
+    });
+    this.server.create("song", {
+      title: "No One Loves Me & Neither Do I",
+      rating: 5,
+      band
+    });
+
+    await visit("/");
+    await click("[data-test-rr=band-link]");
+    await fillIn("[data-test-rr=search-box]", "no");
+
+    assert.equal(currentURL(), "/bands/1/songs?s=no");
+
+    assert
+      .dom("[data-test-rr=song-list-item]")
+      .exists({ count: 2 }, "The songs matching the search term are displayed");
+
+    await click("[data-test-rr=sort-by-title-desc]");
+
+    assert.ok(currentURL().includes("s=no"));
+    assert.ok(currentURL().includes("sort=titleDesc"));
+
+    assert
+      .dom("[data-test-rr=song-list-item]:first-child")
+      .hasText(
+        "No One Loves Me & Neither Do I",
+        "A matching song that comes later in the alphabet appears on top"
+      );
+    assert
+      .dom("[data-test-rr=song-list-item]:last-child")
+      .hasText(
+        "Mind Eraser, No Chaser",
+        "A matching song that comes sooner in the alphabet appears at the bottom"
+      );
   });
 });
